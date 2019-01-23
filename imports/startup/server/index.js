6139f3d0da3b21b9847dd2e45c248339899ae309
domobritton <domonicbritton@gmail.com>
2019-01-23 15:45:16 -0800

    Setup apollo with react and graphql

---
+++ imports/startup/server/index.js
@@ -0,0 +1,25 @@
+import { createApolloServer } from 'meteor/apollo';
+import { makeExecutableSchema } from 'graphql-tools';
+
+// simple schema to define
+const typeDefs = `
+    type Query {
+       hi: String 
+    }
+`;
+
+// resolver to hit the api
+const resolvers = {
+    Query: {
+        hi() {
+            return 'Hello Graphql'
+        }
+    }
+};
+
+const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers
+});
+
+createApolloServer({schema});

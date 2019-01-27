0af611f00228bdb57a9eee740289ba13c19056f8
domobritton <domonicbritton@gmail.com>
2019-01-26 23:46:29 -0800

    Custom resolver to remap data to a new property

--- imports/api/users/resolvers.js
+++ imports/api/users/resolvers.js
@@ -3,5 +3,11 @@ export default {
         user(obj, args, { user }) {
             return user || {};
         }
+    },
+    // pass in the object that we are resolving 'user'
+    // dont have to worry about the shape of the data, just
+    // remap it to a new property below
+    User: {
+        email: user => user.emails[0].address      
     }
-}
+};

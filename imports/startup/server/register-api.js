a9824abd6df8d50f0cb46f8ed211c263f90c616f
domobritton <domonicbritton@gmail.com>
2019-01-26 23:35:38 -0800

    Extend schema types

--- imports/startup/server/register-api.js
+++ imports/startup/server/register-api.js
@@ -4,27 +4,32 @@ import merge from 'lodash/merge';
 
 import ResolutionsSchema from '../../api/resolutions/Resolutions.graphql';
 import ResolutionsResolvers from '../../api/resolutions/resolvers';
-// h
+import UsersSchema from '../../api/users/User.graphql';
+import UsersResolvers from '../../api/users/resolvers';
+// hlffe
 // this pattern is scalable and is good for adding many schemas
-const testSchema = `
-    type Query {
-        hi: String
-        resolutions: [Resolution]
-    }
-`;
+// const testSchema = `
+//     type Query {
+//         hi: String
+//         resolutions: [Resolution]
+//     }
+// `;
 // simple schema to define
-const typeDefs = [testSchema, ResolutionsSchema];
+const typeDefs = [
+    ResolutionsSchema,
+    UsersSchema
+];
 
 // resolver to hit the api
-const testResolvers = {
-    Query: {
-        hi() {
-            return 'Hello Graphql';
-        }
-    }
-};
+// const testResolvers = {
+//     Query: {
+//         hi() {
+//             return 'Hello Graphql';
+//         }
+//     }
+// };
 
-const resolvers = merge(testResolvers, ResolutionsResolvers);
+const resolvers = merge(ResolutionsResolvers, UsersResolvers);
 
 const schema = makeExecutableSchema({
     typeDefs,
